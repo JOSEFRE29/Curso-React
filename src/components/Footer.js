f0d5dbf30221b8854a91808bd752f681af899138
JOSEFRE29 <jose.freire.alvarado@udla.edu.ec>
2025-11-24 18:13:58 -0500

    Subida proyecto a GitHub

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../styles.css';
+
+export default function Footer() {
+    
+    const currrentYear = new Date().getFullYear();  
+    return (
+        <footer className="footer">
+            <p className="footer-text">
+                © {currrentYear} Moviedux. All rights reserved.
+            </p>
+        </footer>
+    );
+}
